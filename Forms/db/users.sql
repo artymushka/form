724c3cefc24ece697375a357cf67b107942f0858
Vlad_Artym <vladartym1@gmail.com>
2024-04-10 17:38:54 +0300

    add commit

---
+++ Forms/db/users.sql
@@ -0,0 +1,45 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Forms/home.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/src/helpers.php';
+
+$user = currentUser();
+?>
+
+<!DOCTYPE html>
+<html lang="ru" data-theme="light">
+<?php include_once __DIR__ . '/components/head.php'?>
+<body>
+
+<div class="card home">
+    <img
+        class="avatar"
+        src="<?php echo $user['avatar'] ?>"
+        alt="<?php echo $user['name'] ?>"
+    >
+    <h1>Привет, <?php echo $user['name'] ?>!</h1>
+    <form action="/Forms/src/actions/logout.php" method="post">
+        <button role="button">Выйти из аккаунта</button>
+    </form>
+</div>
+
+<?php include_once __DIR__ . '/components/script.php' ?>
+</body>
+</html>
